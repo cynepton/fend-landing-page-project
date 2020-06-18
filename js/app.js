51037b304f56e9f4bbbb5e98a43a52cdbabd74b9
isaac <aderonmuisaac@gmail.com>
2020-06-18 21:35:53 +0100

    feat: detect section in viewport
    Added capability to automatically detect the section currently in view

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  *JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
@@ -25,18 +25,18 @@ const sections = document.querySelectorAll("section");
 // Number of sections
 const secLength = sections.length;
 
-// selects the navbar's unordered list tag within which the <li> tags would be placed 
+// selects the navbar's unordered list tag within which the <li> tags would be placed
 const listParent = document.querySelector("#navbar__list");
 
 /**
  * Helper Functions
- * 
+ *
 */
 
 
 /**
  * Main Functions
- * 
+ *
 */
 function addListItem (iterator) {
     const sectionAnchor = document.createElement('a');
@@ -45,7 +45,7 @@ function addListItem (iterator) {
 
     listParent.appendChild(sectionAnchor);
     const listItem = document.createElement('li');
-    listItem.setAttribute('id', `item${iterator}`)
+    listItem.setAttribute('id', `listItem${iterator}`)
     listItem.textContent = `${iterator}`;
 
     sectionAnchor.appendChild(listItem);
@@ -63,7 +63,55 @@ function getTopDistanceArray() {
     return topDistances;
 }
 
+/**
+ * This function takes the topDistances array and uses it to find which one is the active class and then populates the right section with this value
+ */
+function setActiveClass() {
+    let activeIndex = 1;
+    let topDistanceArray = getTopDistanceArray();
+    let activeRegionBottom = 0.6 * window.innerHeight;
+    let activeRegionTop = -0.4 * window.innerHeight;
+    // let viewportHeight = window.innerHeight;
 
+    for (let i = 0; i < topDistanceArray.length; i++) {
+        if (topDistanceArray[i] > activeRegionTop) {
+            // activeIndex = i+1
+            if (topDistanceArray[i] < activeRegionBottom) {
+                activeIndex = i+1                
+            } 
+        }
+    }
+    let activeSection = document.querySelector(`#section${activeIndex}`);
+    let activeNavList = document.querySelector(`#listItem${activeIndex}`);
+    // console.log(activeIndex);
+    for (let i = 0; i < sections.length; i++) {
+        if (i+1 == activeIndex) {
+            activeSection.classList.add('your-active-class');
+            activeNavList.classList.add('activeNavItem');
+        } else {
+            document.querySelector(`#section${i+1}`).classList.remove('your-active-class');
+            document.querySelector(`#listItem${i+1}`).classList.remove('activeNavItem');
+        }
+        
+    }
+    
+}
+
+/*if (topDistanceArray[i] > -1*activeRegion) {
+    if (topDistanceArray[i] < activeClass) {
+        activeClass = topDistanceArray[i];
+        index = i + 1;
+
+        activeSection = document.querySelector(`#section${index}`);
+        activeNavList = document.querySelector(`#listItem${index}`);
+
+        activeSection.classList.add('your-active-class');
+        activeNavList.classList.add('activeNavItem');
+    }
+} else {
+    document.querySelector(`#section${i+1}`).classList.remove('your-active-class');
+    document.querySelector(`#listItem${i+1}`).classList.remove('activeNavItem');
+}*/
 
 // build the nav
 for (let i = 0; i < secLength; i++) {
@@ -73,7 +121,7 @@ for (let i = 0; i < secLength; i++) {
 /**
  * Event listeners
 */
-document.addEventListener('scroll', getSectionInView());
+document.addEventListener("scroll", setActiveClass);
 
 // Add class 'active' to section when near top of viewport
 
@@ -83,7 +131,7 @@ document.addEventListener('scroll', getSectionInView());
 
 
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
